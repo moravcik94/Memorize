41e54840f0cdd488178df7cfa766305683a91a5b
Jakub Moravčík <moravcik94@gmail.com>
2023-04-04 18:30:23 +0200

    after first lecture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3)
+            Text("Hello world!")
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Memorize/ContentView1.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView1.swift
+//  Memorize
+//
+//  Created by Jakub Moravčík on 04/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView1_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView1()
+    }
+}
