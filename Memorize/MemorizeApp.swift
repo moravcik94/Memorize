cb18b6134ea6dde1d19df0216ace50f325b575cf
Jakub Moravčík <moravcik94@gmail.com>
2023-04-04 17:28:46 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jakub Moravčík on 04/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
